f494cd1aad28297379546a2fc05a210b8314fcb0
Lukasz <lukaszskuzjus@gmail.com>
2017-08-25 14:29:44 +0200

    nie dziala ten plik

---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    jshint: {
+      all: ['js/*.js']
+    },
+    sass: {
+  		options: {
+  			sourceMap: true
+  		},
+  		dist: {
+  			files: {
+  				'css/main.css': 'sass/sass_file.sass'
+  			}
+  		}
+  	},
+    
+   watch: {
+    		sass: {
+        		files: ['sass/*.sass'],
+        		tasks: ['sass'],
+        		options: {
+            			spawn: false,
+        		}
+    		},
+		jshint: {
+        		files: ['js/*.js'],
+        		tasks: ['jshint'],
+        		options: {
+            			spawn: false,
+        		},
+    		}
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ["jshint","sass","watch"]);
+
+};
